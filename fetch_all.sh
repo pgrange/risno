a83b752b073cf485b45898dec6d053c3759fa651
Pascal Grange <pascal@grange.nom.fr>
2013-09-13 23:23:16 +0200

    IMP tout en parallèle

--- fetch_all.sh
+++ fetch_all.sh
@@ -5,8 +5,6 @@ do
   python fetch_pubs.py --paru-vendu --logic-immo --se-loger --le-bon-coin $location &
 done
 
-wait
-
 for location in 33830 33380 33160 33980 33480 33680 33138 33680 33740 33950 33970 33510
 do
   python fetch_pubs.py --paru-vendu --logic-immo --se-loger --le-bon-coin $location &
