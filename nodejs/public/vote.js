542ba99e0d364a77bd9b02122706e3d341132e12
Pascal Grange <pascal@grange.nom.fr>
2013-09-22 19:50:03 +0200

    nicer on attend le chargement du document avant de commencer

--- nodejs/public/vote.js
+++ nodejs/public/vote.js
@@ -1,14 +1,16 @@
-$(".btn").click(vote)
-
-function vote() {
-  var opinion = $(this).hasClass('btn-success') 
-    ? "like" : "dislike"
-  var tr = $(this).parents('tr')
-  var id = tr.attr('id')
-
-  $.post("pub/" + id, {opinion: opinion}, function(data) {
-    tr.removeClass("like")
-    tr.removeClass("dislike")
-    tr.addClass(data.opinion)
-  })
+function init_vote() {
+  $(".btn").click(vote)
+  
+  function vote() {
+    var opinion = $(this).hasClass('btn-success') 
+      ? "like" : "dislike"
+    var tr = $(this).parents('tr')
+    var id = tr.attr('id')
+  
+    $.post("pub/" + id, {opinion: opinion}, function(data) {
+      tr.removeClass("like")
+      tr.removeClass("dislike")
+      tr.addClass(data.opinion)
+    })
+  }
 }
