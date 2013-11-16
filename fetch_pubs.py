5302ac273a2a6a256c23b21d7678b8ac909bd2c7
Pascal Grange <pascal@grange.nom.fr>
2013-11-16 17:36:08 +0100

    ADD le site à vendre à louer

--- fetch_pubs.py
+++ fetch_pubs.py
@@ -19,16 +19,20 @@ def insert_to_db(pubs):
   from pyes import ES
   conn = ES('127.0.0.1:9200') # Use HTTP
 
-  if len(pubs) == 0:
-    log("WA", "no pub, we may have been blacklisted")
-  
   for pub in pubs: conn.update("test-index", "test-type", pub['id'], document=pub['object'], upsert=pub['object'])
+
+def show_pubs(pubs):
+  for pub in pubs:
+    print pub
 
 def image_to_id(image_url):
    try:
      return str(hashlib.md5(urllib2.urlopen(image_url).read()).hexdigest())
    except urllib2.URLError as err:
      log("WA", "Unable to generate id from image, generating from image url instead: " + str(err.reason))
+     return "default-" + hashlib.md5(image_url).hexdigest()
+   except ValueError as err:
+     log("WA", "Unable to generate id from image, generating from image url instead: " + str(err))
      return "default-" + hashlib.md5(image_url).hexdigest()
 
 class LogicImmo:
@@ -88,6 +92,48 @@ class LogicImmo:
     if not self.logic_crap.has_key(location): raise Exception("I do not know this location sorry: " + location)
     crap = self.logic_crap[location]
     return "http://www.logic-immo.com/vente-immobilier-" + crap[0] + "-" + location + "," + crap[1] + "-4f2f000000-0,200000-0,0-0,0-00-00-000000000000-00-0-0-3-0-0-1.html"
+
+class AVendreALouer:
+  
+  def __init__(self):
+    self.name = 'a-vendre-a-louer'
+
+  def parse(self, page):
+    soup = BeautifulSoup(page)
+    
+    pubs = []
+    for li in soup.find_all('li', 'resultat'):
+      href = li.find('a', 'annonce_url').attrs[u'href']
+      img = li.find('div', 'photo').find('img')
+      placement = li.find('div', 'titre').find('h2')
+      price = li.find('div', 'prix')
+      details = li.find('div', "descriptif").get_text().replace('\n', '').replace('\r', '').replace('  ', ' ')
+
+      if (not href or not img or not placement or not price):
+        continue
+
+      img = img.attrs[u'src']
+      potential_price = re.findall('.*[0-9]', price.get_text())
+      if len(potential_price)> 0:
+        price = potential_price[0]
+      else:
+        price = price.get_text()
+
+      pubs.append(
+        { #'id': '1-' + re.search("(?<=ventes_immobilieres/)[0-9]+", href).group(0),
+          'id': image_to_id(img),
+          'object': 
+            { 'url': href, 
+              'img': img,
+              'placement': placement.get_text().replace('\n', '').replace(' ', ''),
+              'price': price,
+              'details': details
+            }})
+    
+    return pubs
+
+  def search_url(self, location): 
+    return "http://www.avendrealouer.fr/annonces-immobilieres/vente/appartement+maison/" + location + "+cp/max-300000-euros"
 
 class LeBonCoin:
   
@@ -199,10 +245,12 @@ if __name__ == '__main__':
   import argparse
 
   parser = argparse.ArgumentParser(description='récupère les annonces pour le site et le code postal précisé')
+  parser.add_argument('--test', const=True, action='store_const', help='affiche les annonces sans les stocker en base')
   parser.add_argument('--le-bon-coin', const=True, action='store_const', help='recherche sur le bon coin')
   parser.add_argument('--logic-immo',  const=True, action='store_const', help='recherche sur logic immo')
   parser.add_argument('--se-loger',    const=True, action='store_const', help='recherche sur se loger')
   parser.add_argument('--paru-vendu',  const=True, action='store_const', help='recherche sur paru vendu')
+  parser.add_argument('--avendre-alouer',  const=True, action='store_const', help='recherche sur à vendre à louer')
   parser.add_argument('locations', nargs=argparse.REMAINDER, help='code postaux où recherche les annones')
   args = parser.parse_args()
   
@@ -212,6 +260,7 @@ if __name__ == '__main__':
   if args.le_bon_coin: sites.append(LeBonCoin())
   if args.paru_vendu: sites.append(ParuVendu())
   if args.se_loger: sites.append(SeLoger())
+  if args.avendre_alouer: sites.append(AVendreALouer())
   
   for site in sites:
     for location in args.locations:
@@ -220,7 +269,10 @@ if __name__ == '__main__':
         url = site.search_url(location)
         page = urllib2.urlopen(url)
         pubs = site.parse(page)
-        insert_to_db(pubs)
+	if len(pubs) == 0:
+	  log("WA", "no pub, we may have been blacklisted")
+        if args.test: show_pubs(pubs)
+        else: insert_to_db(pubs)
         log("OK")
       except:
         #log("KO", traceback.format_exc().splitlines()[-1])
