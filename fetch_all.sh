967c3c0b5b1769aa36361abef8779a270a06a60d
Pascal Grange <pascal@grange.nom.fr>
2014-01-08 19:18:09 +0100

    FIX restore existing list of postal code and limit number of     parallel fetches

--- fetch_all.sh
+++ fetch_all.sh
@@ -2,28 +2,18 @@
 
 . etc/risnorc
 
-#for location in 33114 33125 33650 33720 33730 33770 33830 40160 40210 40410 40460
+nb_running=0
 for location in $POSTAL_CODE
 do
   python fetch_pubs.py --avendre-alouer --paru-vendu --logic-immo --se-loger --le-bon-coin --pages-jaunes --immo-street $location &
-done
-
-wait
-python update_locations.py
-
-#for location in 33830 33380 33160 33980 33480 33680 33138 33680 33740 33950 33970 33510
-for location in $POSTAL_CODE
-do
-  python fetch_pubs.py --avendre-alouer --paru-vendu --logic-immo --se-loger --le-bon-coin --pages-jaunes --immo-street $location &
-done
-
-wait
-python update_locations.py
-
-#for location in 40110 40160 40170 40200 40210 40260 40560 40600
-for location in $POSTAL_CODE
-do
-  python fetch_pubs.py --avendre-alouer --paru-vendu --logic-immo --se-loger --le-bon-coin --pages-jaunes --immo-street $location &
+  nb_running=$((nb_running + 1))
+  if [ $nb_running -eq 8 ]
+  then
+    # 8 parallel fetches max
+    nb_running=0
+    wait
+    python update_locations.py
+  fi
 done
 
 wait
