bcee797fe40f61a7de50dcd0e7b3a2338acbe626
Pascal Grange <pascal@grange.nom.fr>
2014-01-12 12:27:57 +0100

    Run region fetch in parrallel

--- fetch_all.sh
+++ fetch_all.sh
@@ -6,20 +6,26 @@
 nb_running=0
 for location in $POSTAL_CODE
 do
-  python fetch_pubs.py --avendre-alouer --paru-vendu --logic-immo --se-loger --le-bon-coin --pages-jaunes --immo-street --zip $location &
+  python -u fetch_pubs.py --avendre-alouer --paru-vendu --logic-immo --se-loger --le-bon-coin --pages-jaunes --immo-street --zip $location &
   nb_running=$((nb_running + 1))
   if [ $nb_running -eq 8 ]
   then
     # 8 parallel fetches max
     nb_running=0
     wait
-    python update_locations.py
+    python -u update_locations.py
   fi
 done
 wait
-python update_locations.py
+python -u update_locations.py &
 
 #fetching for region
-python fetch_pubs.py --avendre-alouer --paru-vendu --logic-immo --se-loger --le-bon-coin --pages-jaunes --immo-street --region $REGION 
+(python -u fetch_pubs.py --avendre-alouer --region $REGION; python -u update_locations.py) &
+(python -u fetch_pubs.py --logic-immo --region $REGION; python -u update_locations.py) &
+(python -u fetch_pubs.py --se-loger --region $REGION; python -u update_locations.py) &
+(python -u fetch_pubs.py --le-bon-coin --region $REGION; python -u update_locations.py) &
+(python -u fetch_pubs.py --pages-jaunes --region $REGION; python -u update_locations.py) &
+(python -u fetch_pubs.py --immo-street --region $REGION; python -u update_locations.py) &
 
-python update_locations.py
+wait
+python -u update_locations.py
