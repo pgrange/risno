c1edc58a0b194add4067c1d6c2b89d658593f499
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2015-05-28 14:09:43 +0200

    Typo
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- nodejs/index.js
+++ nodejs/index.js
@@ -11,7 +11,8 @@ nconf.argv()
      .file({ file: '/etc/risno.json' })
      .defaults({ listen_port: 12043,
                  // elastic_db: '9200'})
-                 elastic_db: process.env.ELASTICSEARCH_1_PORT_9200_TCP_PORT || 9200})
+                 elastic_db: process.env.ELASTICSEARCH_1_PORT_9200_TCP_PORT
+                          || 'http://localhost:9200'})
 
 // Debug
 console.log('ES: ' + nconf.get('elastic_db'));
