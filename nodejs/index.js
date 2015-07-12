1fa9c9d69dd2759d426748477d119648b1c3355b
Pascal Grange <pascal@grange.nom.fr>
2015-07-12 21:55:22 +0200

    FIX elasticsearch configuration system for web application:     use environment variable and defaults to localhost:9200

--- nodejs/index.js
+++ nodejs/index.js
@@ -6,18 +6,11 @@ var crypto = require('crypto')
 var nconf = require('nconf')
 var nodemailer = require('nodemailer')
 
-
-var es_port = 'localhost:9200'
-if (process.env.ELASTICSEARCH_PORT_9200_TCP_ADDR)
-  es_port = process.env.ELASTICSEARCH_PORT_9200_TCP_ADDR
-          + ':'
-          + process.env.ELASTICSEARCH_PORT_9200_TCP_PORT
-
 nconf.argv()
      .env()
      .file({ file: '/etc/opt/risno.json' })
      .defaults({ listen_port: 12043,
-                 elastic_db: es_port})
+                 elastic_db: 'localhost:9200'})
 
 var app = express()
 app.locals.pretty = true
